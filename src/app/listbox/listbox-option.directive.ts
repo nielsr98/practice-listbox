55604b380ba40f35e576e88584254959f9913911
niels <nielsr@google.com>
2020-05-31 23:03:47 -0400

    Added an Output to ListboxOption so it can communicate to the listbox upon selection. Unclear if this is the best moving forward. Added functions to handle the selection of options.

--- src/app/listbox/listbox-option.directive.ts
+++ src/app/listbox/listbox-option.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Input} from '@angular/core';
+import {Directive, ElementRef, EventEmitter, HostListener, Input, Output} from '@angular/core';
 import {ListKeyManagerOption} from '@angular/cdk/a11y/';
 
 @Directive({
@@ -17,10 +17,24 @@ export class ListboxOptionDirective implements ListKeyManagerOption {
 
   @Input() label: string;
 
+  @Output() selectionChange = new EventEmitter<ListboxOptionDirective>();
+
+  @HostListener('click') onClick() {
+    this.selected = !this.selected;
+    this.selectionChange.emit(this);
+    if (this.selected) {
+      this.focusOption();
+    }
+  }
+
   private disable?: boolean;
 
   getLabel?(): string {
     return this.label;
+  }
+
+  focusOption() {
+    this.el.nativeElement.focus();
   }
 
 }
--- src/app/listbox/listbox.directive.ts
+++ src/app/listbox/listbox.directive.ts
@@ -6,7 +6,8 @@ import {
   Input,
   Output,
   QueryList,
-  ViewChildren
+  ViewChildren,
+  AfterViewInit
 } from '@angular/core';
 import {ListboxOptionDirective} from "./listbox-option.directive";
 import {ListKeyManager} from "@angular/cdk/a11y";
@@ -26,5 +27,13 @@ export class ListboxDirective {
 
   private _listKeyManager: ListKeyManager<ListboxOptionDirective>;
 
+  ngAfterViewInit() {
+    this._listKeyManager = new ListKeyManager<ListboxOptionDirective>(this._options)
+                               .withWrap().withVerticalOrientation(true);
+  }
 
+  onSelectedOptionChange(option: ListboxOptionDirective) {
+    this.selectedOption = this.selectedOption === option ? undefined : option;
+    this.selectedIndex = this.selectedOption === option ? -1 : this._options.toArray().indexOf(option);
+  }
 }
